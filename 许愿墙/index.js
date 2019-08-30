c9201a553dd043834fbc610bbbf3a9a0efbd648d
smilele <18958849752@163.com>
2019-08-31 00:13:16 +0800

    许愿墙初始

---
+++ 许愿墙/index.js
@@ -0,0 +1,40 @@
+;
+(function () {
+    var wishingWallConfig = {
+
+    }
+
+    initWish();
+
+    AddWishOfInp();
+
+    /**
+     * input 许愿
+     */
+    function AddWishOfInp() {}
+
+
+    /**
+     * 创建一个愿望
+     */
+    function createOneWish(
+
+    );
+
+
+    /**
+     * 初始化愿望
+     */
+    function initWish() {}
+}());
+
+var betterFunctions = {
+    /**
+     * 随机范围整数
+     * @param {Number} min 最小值
+     * @param {Number} max 最大值
+     */
+    getRandomNumber: function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+}
